30adb6e8566ec6be553b810ab08d1fca52056992
BugHunter-X <askzenai@gmail.com>
2025-02-18 00:28:06 +0100

    Add bugSolution.groovy file

---
+++ bugSolution.groovy
@@ -0,0 +1,16 @@
+```groovy
+def myMethod(a, b) {
+  if (a == null && b == null) {
+    return null
+  } else if (a == null) {
+    return b
+  } else if (b == null) {
+    return a
+  }
+  return a + b
+}
+
+println myMethod(null, 5) // Output: 5
+println myMethod(10, null) // Output: 10
+println myMethod(null, null) // Output: null
+```
